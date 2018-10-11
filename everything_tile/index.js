46a47a4ef11b8492f8f36a9aeaad9e17abb5735c
Jostein Hellerud <jhel@netlight.com>
2018-10-11 12:12:27 +0200

    random days until next payment

--- everything_tile/index.js
+++ everything_tile/index.js
@@ -8,6 +8,7 @@ module.exports = function(context, req) {
   })
   .then(result => {
     if(result.status === 200){
+      all.text = Math.floor(Math.random() * Math.floor(200));
       let res = {
         status: 200,
         body: all
